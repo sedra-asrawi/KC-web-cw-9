f2960ff2798aff8dc900bfe1a0b83d98df818f22
sedra-asrawi <sedraasrawi@gmail.com>
2022-11-05 19:11:53 +0300

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+orders = []
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +8,22 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder = {
+name,
+id,
+order,
+phone
+
+  }
   // أدرج الكائن إلى المصفوفة
-  
+  order.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()
 }
 
 function load() {
   let container = document.getElementById("container");
   container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
-
+  `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
 }
